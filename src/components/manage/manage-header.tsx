b0c9053c5ccfe32b917499428e45462a031bdec4
latin1gor <latinigor.main@gmail.com>
2024-07-07 21:48:57 +0300

    fixed manage header bg

--- src/components/manage/manage-header.tsx
+++ src/components/manage/manage-header.tsx
@@ -1,13 +1,17 @@
 const ManageHeader = () => {
   return (
-    <div
-      className={
-        "container flex flex-wrap items-center justify-between gap-6 py-8"
-      }
-    >
-      <div>
-        <p className={"text-3xl font-bold"}>Manage</p>
-        <p className={"text-muted-foreground"}>Manage your account settings</p>
+    <div className={" border-b bg-card"}>
+      <div
+        className={
+          "container flex flex-wrap items-center justify-between gap-6 py-8"
+        }
+      >
+        <div>
+          <p className={"text-3xl font-bold"}>Manage</p>
+          <p className={"text-muted-foreground"}>
+            Manage your account settings
+          </p>
+        </div>
       </div>
     </div>
   );
