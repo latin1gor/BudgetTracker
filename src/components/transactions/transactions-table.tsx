34b8d9fa69a32e80a59f2f1f120a32b12bd69e1e
latin1gor <latinigor.main@gmail.com>
2024-07-09 12:31:39 +0300

    setup transaction table

---
+++ src/components/transactions/transactions-table.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { FromToType } from "@/lib/types";
+
+const TransactionsTable = ({ from, to }: FromToType) => {
+  return <>Transaction Table</>;
+};
+
+export default TransactionsTable;
